03f284fb9cdab22b110e171f2639cb3c214db28e
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-01 14:59:04 -0800

    cleaned up article.js

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -26,7 +26,6 @@ Article.prototype.toHtml = function() {
 
   $newArticle.append('<hr>');
 
-  // TODO: This cloned article is no longer a template, so we should remove that class...
   $('article').removeClass('template');
 
   return $newArticle;
